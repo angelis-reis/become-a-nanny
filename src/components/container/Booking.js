f8c5e4a975e26fcc9cfcf54293aba299dabfb7d9
angelis-reis <angelisreisbiof@gmail.com>
2021-06-12 12:30:20 -0300

    feat: add booking section

--- src/components/container/Booking.js
+++ src/components/container/Booking.js
@@ -1,20 +1,40 @@
 import React from 'react';
+import { BrowserRouter as Router, Link } from 'react-router-dom';
 
 import styled from 'styled-components';
+import ProfileImage from '../../utils/ProfileImage.svg';
 
 const BookingStyle = styled.div`
 	height: 112px;
 	/* width: 1440px; */
-	left: 0px;
-	top: 0px;
-	border-radius: 0px;
-	border: 2px solid black;
+
+	display: flex;
+	/* align-self: center; */
+	justify-content: center;
+	align-items: center;
+
+
+	.profile-img {
+		width: 56px;
+		height: 56px;
+		margin-right: 32px;
+	}
 `;
 
 function Booking() {
 	return (
 		<BookingStyle>
-			<h1>Booking</h1>
+			<Router>
+				<img
+					className='profile-img'
+					src={ProfileImage}
+					alt='User Profile'
+				/>
+				<Link className='action-link' to='/play'>
+					<span>See hapu in action (27 seconds)</span>
+				</Link>
+				<h4>Wednesday, Thursday, Friday - 7:30 - 5:30</h4>
+			</Router>
 		</BookingStyle>
 	);
 }
