15b886108d82e6474923be1f87fb68efbecd3c67
angelis-reis <angelisreisbiof@gmail.com>
2021-06-15 22:57:43 -0300

    style: fix create nanny share button style

--- src/components/container/Host.js
+++ src/components/container/Host.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Link } from 'react-router-dom';
-
 import styled from 'styled-components';
-import ButtonHost from '../../images/Button.png';
+import CalendarIcon from '../../utils/CalendarIcon.svg';
 
 const HostStyle = styled.div`
 	height: 322px;
 	left: 0px;
 	top: 0px;
 	background: #ffffff;
-
-
 
 	.Host-text {
 		width: 784px;
@@ -23,6 +20,55 @@ const HostStyle = styled.div`
 		text-align: center;
 		place-items: center;
 	}
+
+	.host-button {
+		width: 304px;
+		height: 68px;
+		border-radius: 4px;
+		border: none;
+		background-color: #5e20a4;
+		color: #ffffff;
+		cursor: pointer;
+		display: grid;
+		place-items: center;
+		grid-template-columns: 70px 1fr;
+		grid-template-rows: 1fr 1fr;
+		grid-template-areas:
+			'calendar-icon button-text'
+			'calendar-icon button-subtext';
+	}
+
+	.create-link {
+		text-decoration: none;
+	}
+
+	.host-button span {
+		color: #ffffff;
+	}
+
+	.calendar-icon {
+		grid-area: calendar-icon;
+		width: 32px;
+		height: 32px;
+	}
+
+	.button-text {
+		grid-area: button-text;
+		font-size: 16px;
+		line-height: 28px;
+		font-weight: 400;
+		justify-self: start;
+		align-self: end;
+		margin-top: 10px;
+	}
+	.button-subtext {
+		grid-area: button-subtext;
+		font-size: 12px;
+		line-height: 16px;
+		font-weight: 400;
+		justify-self: start;
+		align-self: start;
+	}
 `;
 
 function Host() {
@@ -34,13 +80,24 @@ function Host() {
 					<p className='Host-text'>
 						Takes less than 5 minutes to get started
 					</p>
-					<Link className='start-link' to='/login'>
-						<img
-							src={ButtonHost}
-							alt='Create a Nanny Share Button'
-						/>
+					<Link className='create-link' to='/login'>
+						<div className='host-button'>
+							<img
+								className='calendar-icon'
+								src={CalendarIcon}
+								alt='Nanny Share Calendar Button'
+							/>
+
+							<span className='button-text'>
+								Create your nanny share
+							</span>
+							<span className='button-subtext'>
+								Takes less than 5 minutes
+							</span>
+						</div>
 					</Link>
-					<Link className='start-link' to='/browse'>
+
+					<Link to='/browse'>
 						<span>Or browse local nanny-shares</span>
 					</Link>
 				</div>
